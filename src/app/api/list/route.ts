283efb406561931e91f897981f8cf6b11d14afda
Noor Elahi <noorellahi220@gmail.com>
2026-05-17 20:03:03 +0500

    Added GET api

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -1,9 +1,64 @@
 
 import { verifyToken } from "@/lib/jwt"
 import { prisma } from "@/lib/prisma"
+
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 import z from "zod"
+
+export async function GET(req: Request) {
+
+
+    try {
+        const cookieStore = await cookies()
+        const token = cookieStore.get('token')?.value
+
+        if (!token) {
+            return new NextResponse(
+                JSON.stringify({
+                    error: "Unauthorized!"
+                }),
+                {status:401}
+            )
+        }
+
+        let decoded: any
+
+        try {
+            decoded = verifyToken(token)
+        } catch (error) {
+            return new NextResponse(
+                JSON.stringify({
+                    error: "Inv token!"
+                }),
+                { status: 401 }
+            )
+        }
+
+
+        const findAll = await prisma.list.findMany({
+            where: {
+                userId: decoded.id
+            }
+        })
+
+        return new NextResponse(
+            JSON.stringify({
+                list: findAll
+            }),
+            { status: 200 }
+        )
+
+    } catch (error) {
+        return new NextResponse(
+            JSON.stringify({ message: "An error occurred while fetching list", error: error instanceof Error ? error.message : String(error) }),
+            { status: 500 }
+        )
+    }
+}
+
+
+
 
 
 export async function POST(req: Request) {
