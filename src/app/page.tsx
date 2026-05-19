88afde4f1b1c30c905833c1dd6814a1a8619e39a
Noor Elahi <noorellahi220@gmail.com>
2026-05-19 16:52:15 +0500

    Working on Ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,29 @@
-
-import { prisma } from "@/lib/prisma";
+'use client'
+import { useState } from "react";
 import { BiMenu, BiPlus, BiCheckbox, BiCheckboxChecked, BiChevronRight } from "react-icons/bi";
 
 
 
 
-const Home = async () => {
-  // await prisma.test.create({
-  //   data: {
-  //     name: "Its working  DB connected"
-  //   },
-  // })
+const Home = () => {
 
-  // const tests = await prisma.test.findMany();
+  const [menu , setMenu] = useState(false)
+
+
   return (
     <div className="h-screen w-full flex justify-center items-center bg-gray-400">
 
-      <div className="w-[80%] flex rounded-tr-xl rounded-tl-xl p-5 bg-[#FAFAFA] h-[90%]">
-        <div className="w-[20%] p-5  bg-[#F2F2F2]">
+      <div className={`w-[80%] flex rounded-tr-xl ${menu ? "" : "p-5"} rounded-tl-xl bg-[#FAFAFA] h-[90%]`}>
+        <div className={`w-[25%] p-5 ${menu ? 'hidden' : ''}  bg-[#F2F2F2]`}>
           <div className="flex justify-between">
             <h3 className="text-xl">Menu</h3>
-            <BiMenu className="text-3xl text-[#7C7C7C]" />
+            <BiMenu className="text-3xl text-[#7C7C7C]" onClick={() => setMenu(!menu)}/>
           </div>
         </div>
-        <div className="h-full w-[80%] bg-gray-500">
-          <div className="pl-10 pt-9">
+        <div className="h-full w-[100%]">
+          <div className={`pl-10 pt-9 ${menu ? '' : 'pl-0 pt-[0px]'}`}>
             <div className="flex gap-25 items-center">
-              <BiMenu className="text-3xl text-[#7C7C7C]" />
+              <BiMenu className={`text-3xl ${menu ? "" : "hidden"} text-[#7C7C7C]`} onClick={() => setMenu(!menu)}/>
               <div className="flex gap-9">
                 <h1 className="text-5xl">Today</h1>
                 <h2 className="text-3xl px-3 py-1 rounded-lg border-2 border-[#F3F3F3]">5</h2>
@@ -35,7 +32,7 @@ const Home = async () => {
           </div>
 
           <div className="h-[90%] pt-10 flex justify-end pr-15">
-            <div className="w-[85%] ">
+            <div className={`w-[85%]  ${menu ? "" : "w-[100%] pl-10"} `}>
               <div className="relative w-full text-[#7c7c7c]">
                 <BiPlus className="absolute left-5 top-1/2 text-lg -translate-y-1/2" />
 
